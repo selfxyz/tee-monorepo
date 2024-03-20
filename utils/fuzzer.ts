597c2cbf27b165367f8e895240af71790c672f5a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-03-20 14:31:09 +0530

    update fuzzer

--- utils/fuzzer.ts
+++ utils/fuzzer.ts
@@ -1,19 +1,18 @@
-import { BigNumber, BigNumberish, utils } from "ethers";
-import { keccak256, randomBytes } from "ethers/lib/utils";
+import { toBigInt, getAddress, keccak256, randomBytes } from "ethers";
 
 export class FuzzedNumber {
-  static random(): BigNumber {
+  static random(): bigint {
     const bytes = randomBytes(32);
-    return BigNumber.from(bytes);
+    return toBigInt(bytes);
   }
 
-  static randomInRange(min: BigNumberish, max: BigNumberish): BigNumber {
-    if (BigNumber.from(max).lte(min)) {
+  static randomInRange(min: bigint, max: bigint): bigint {
+    if (max <= min) {
       throw new Error("max should be more than min");
     }
     const bytes = randomBytes(32);
-    const diff = BigNumber.from(max).sub(min);
-    return BigNumber.from(bytes).mod(diff).add(min);
+    const diff = max - min;
+    return toBigInt(bytes) % diff + min;
   }
 }
 
@@ -22,6 +21,6 @@ export class FuzzedAddress {
     let address = keccak256(Buffer.from(rand + new Date().valueOf().toString()))
       .toString()
       .slice(0, 42);
-    return utils.getAddress(address);
+    return getAddress(address);
   }
 }
