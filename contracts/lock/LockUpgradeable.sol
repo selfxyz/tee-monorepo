6dabdfa8f7457dc99b90af5a7d256950810f2ec0
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-03-21 18:23:37 +0530

    transition to namespaced storage

--- contracts/lock/LockUpgradeable.sol
+++ contracts/lock/LockUpgradeable.sol
@@ -13,10 +13,20 @@ contract LockUpgradeable is
         uint256 iValue;
     }
 
-    mapping(bytes32 => Lock) public locks;
-    mapping(bytes32 => uint256) public lockWaitTime;
+    /// @custom:storage-location erc7201:marlin.oyster.storage.Lock
+    struct LockStorage {
+        mapping(bytes32 => Lock) locks;
+        mapping(bytes32 => uint256) lockWaitTime;
+    }
 
-    uint256[48] private __gap;
+    // keccak256(abi.encode(uint256(keccak256("marlin.oyster.storage.Lock")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant LockStorageLocation = 0x2ba99b043df9e6db72821769dcc0757c8c1b1dc979dd0c3217f2e035fed90700;
+
+    function _getLockStorage() private pure returns (LockStorage storage $) {
+        assembly {
+            $.slot := LockStorageLocation
+        }
+    }
 
     enum LockStatus {
         None,
@@ -36,8 +46,10 @@ contract LockUpgradeable is
     }
 
     function _lockStatus(bytes32 _selector, bytes32 _key) internal view returns (LockStatus) {
+        LockStorage storage $ = _getLockStorage();
+
         bytes32 _lockId = keccak256(abi.encodePacked(_selector, _key));
-        uint256 _unlockTime = locks[_lockId].unlockTime;
+        uint256 _unlockTime = $.locks[_lockId].unlockTime;
         if (_unlockTime == 0) {
             return LockStatus.None;
         } else if (_unlockTime <= block.timestamp) {
@@ -48,13 +60,15 @@ contract LockUpgradeable is
     }
 
     function _lock(bytes32 _selector, bytes32 _key, uint256 _iValue) internal returns (uint256) {
+        LockStorage storage $ = _getLockStorage();
+
         require(_lockStatus(_selector, _key) == LockStatus.None);
 
-        uint256 _duration = lockWaitTime[_selector];
+        uint256 _duration = $.lockWaitTime[_selector];
         bytes32 _lockId = keccak256(abi.encodePacked(_selector, _key));
         uint256 _unlockTime = block.timestamp + _duration;
-        locks[_lockId].unlockTime = _unlockTime;
-        locks[_lockId].iValue = _iValue;
+        $.locks[_lockId].unlockTime = _unlockTime;
+        $.locks[_lockId].iValue = _iValue;
 
         emit LockCreated(_selector, _key, _iValue, _unlockTime);
 
@@ -62,9 +76,11 @@ contract LockUpgradeable is
     }
 
     function _revertLock(bytes32 _selector, bytes32 _key) internal returns (uint256) {
+        LockStorage storage $ = _getLockStorage();
+
         bytes32 _lockId = keccak256(abi.encodePacked(_selector, _key));
-        uint256 _iValue = locks[_lockId].iValue;
-        delete locks[_lockId];
+        uint256 _iValue = $.locks[_lockId].iValue;
+        delete $.locks[_lockId];
 
         emit LockDeleted(_selector, _key, _iValue);
 
@@ -77,21 +93,25 @@ contract LockUpgradeable is
     }
 
     function _cloneLock(bytes32 _selector, bytes32 _fromKey, bytes32 _toKey) internal {
+        LockStorage storage $ = _getLockStorage();
+
         bytes32 _fromLockId = keccak256(abi.encodePacked(_selector, _fromKey));
         bytes32 _toLockId = keccak256(abi.encodePacked(_selector, _toKey));
 
-        uint256 _unlockTime = locks[_fromLockId].unlockTime;
-        uint256 _iValue = locks[_fromLockId].iValue;
+        uint256 _unlockTime = $.locks[_fromLockId].unlockTime;
+        uint256 _iValue = $.locks[_fromLockId].iValue;
 
-        locks[_toLockId].unlockTime = _unlockTime;
-        locks[_toLockId].iValue = _iValue;
+        $.locks[_toLockId].unlockTime = _unlockTime;
+        $.locks[_toLockId].iValue = _iValue;
 
         emit LockCreated(_selector, _toKey, _iValue, _unlockTime);
     }
 
     function _updateLockWaitTime(bytes32 _selector, uint256 _newLockWaitTime) internal {
-        emit LockWaitTimeUpdated(_selector, lockWaitTime[_selector], _newLockWaitTime);
-        lockWaitTime[_selector] = _newLockWaitTime;
+        LockStorage storage $ = _getLockStorage();
+
+        emit LockWaitTimeUpdated(_selector, $.lockWaitTime[_selector], _newLockWaitTime);
+        $.lockWaitTime[_selector] = _newLockWaitTime;
     }
 
     function _updateLockWaitTimes(bytes32[] memory _selectors, uint256[] memory _newLockWaitTimes) internal {
