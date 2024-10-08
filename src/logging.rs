4f864d0ad19fa3571dc91d86b9e720dcb6f2efae
naman-monga <namanmonga27@gmail.com>
2024-10-09 00:48:19 +0530

    removed redundant permissions for logs file

--- src/logging.rs
+++ src/logging.rs
@@ -2,35 +2,14 @@ use anyhow::Context;
 use chrono::Local;
 use std::fs::OpenOptions;
 use std::io::{ErrorKind, Write};
-#[cfg(unix)]
-use std::os::unix::fs::{OpenOptionsExt, PermissionsExt};
 
 pub fn log_message(log_file: &str, message: &str) -> anyhow::Result<()> {
     // Create OpenOptions
     let mut options = OpenOptions::new();
     options.create(true).append(true);
 
-    // On Unix platforms, set the file mode (permissions) when creating the file
-    #[cfg(unix)]
-    {
-        use std::path::Path;
-        if !Path::new(log_file).exists() {
-            options.mode(0o666);
-        }
-    }
-
     // Open the file with the specified options
     let mut file = options.open(log_file).context("failed to open debug log file")?;
-
-    // On Unix platforms, set the permissions explicitly
-    #[cfg(unix)]
-    {
-        let metadata = file.metadata().context("failed to read file metadata")?;
-        let mut permissions = metadata.permissions().context("failed to read permission from file metadata");
-
-        // Set permissions to 0o666
-        permissions.set_mode(0o666);
-        file.set_permissions(permissions).context("failed to set file permissions")}
 
     // Get the current time in a human-readable format
     let now = Local::now().format("%Y-%m-%d %H:%M:%S");
