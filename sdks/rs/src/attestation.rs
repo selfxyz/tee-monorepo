17466283ec2ae1659e6c6d8cf9459d6c8a43de2b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-17 14:35:19 +0530

    add and expose the aws root key

--- sdks/rs/src/attestation.rs
+++ sdks/rs/src/attestation.rs
@@ -15,6 +15,8 @@ use serde_cbor::{self, value, value::Value};
 
 // TODO: update other programs depending on the sdk
 
+pub const AWS_ROOT_KEY: [u8; 96] = hex_literal::hex!("fc0254eba608c1f36870e29ada90be46383292736e894bfff672d989444b5051e534a4b1f6dbe3c0bc581a32b7b176070ede12d69a3fea211b66e752cf7dd1dd095f6f1370f4170843d9dc100121e4cf63012809664487c9796284304dc53ff4");
+
 #[derive(Debug)]
 pub struct AttestationDecoded {
     pub timestamp: usize,
