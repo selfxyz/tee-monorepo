cef852381c84a873be7ececd1b8bb5bdc78b2b92
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-20 17:26:45 +0530

    add version specifiers

--- sdks/ts/attestation/mod.ts
+++ sdks/ts/attestation/mod.ts
@@ -1,7 +1,7 @@
 import { decodeHex } from "jsr:@std/encoding@1/hex";
-import { decode } from "npm:cbor";
-import * as cose from "npm:cose-js";
-import { X509Certificate } from "npm:@peculiar/x509";
+import { decode } from "npm:cbor@10";
+import * as cose from "npm:cose-js@0.9";
+import { X509Certificate } from "npm:@peculiar/x509@1";
 
 export const AWS_ROOT_KEY: Uint8Array = decodeHex(
   "fc0254eba608c1f36870e29ada90be46383292736e894bfff672d989444b5051e534a4b1f6dbe3c0bc581a32b7b176070ede12d69a3fea211b66e752cf7dd1dd095f6f1370f4170843d9dc100121e4cf63012809664487c9796284304dc53ff4",
