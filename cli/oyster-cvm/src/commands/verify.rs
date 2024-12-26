5c1bd509e65bd46fa83909629e457238960d61da
naman-monga <namanmonga27@gmail.com>
2024-12-26 17:51:41 +0530

    import aws root key from oyster-sdk

--- cli/oyster-cvm/src/commands/verify.rs
+++ cli/oyster-cvm/src/commands/verify.rs
@@ -3,9 +3,7 @@ use hex;
 use std::time::{SystemTime, UNIX_EPOCH};
 use tracing::info;
 
-use oyster::attestation::{get, verify, AttestationExpectations};
-
-pub const AWS_ROOT_KEY: [u8; 96] = hex_literal::hex!("fc0254eba608c1f36870e29ada90be46383292736e894bfff672d989444b5051e534a4b1f6dbe3c0bc581a32b7b176070ede12d69a3fea211b66e752cf7dd1dd095f6f1370f4170843d9dc100121e4cf63012809664487c9796284304dc53ff4");
+use oyster::attestation::{get, verify, AttestationExpectations, AWS_ROOT_KEY};
 
 pub async fn verify_enclave(
     pcr0: &String,
