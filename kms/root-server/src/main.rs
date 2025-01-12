35f0bc6a5bf0ba3fe057ee6736e5db75aaec6ae0
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-12 13:21:22 +0530

    setup logging

--- kms/root-server/src/main.rs
+++ kms/root-server/src/main.rs
@@ -1,18 +1,33 @@
 use anyhow::{Context, Result};
 use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 async fn hello() -> (StatusCode, String) {
     (StatusCode::OK, "Hello, World!".to_owned())
 }
 
+fn setup_logging() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    setup_logging();
+
     let app = Router::new().route("/", get(hello));
 
     let listener = TcpListener::bind("0.0.0.0:1100")
         .await
         .context("failed to bind listener")?;
+
+    info!("Listening on {}", "0.0.0.0:1100");
     axum::serve(listener, app)
         .await
         .context("failed to serve")?;
