27880eeef7b03d6551a5fa91cf9f9652b9251900
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-12 13:13:01 +0530

    basic axum server

--- kms/root-server/src/main.rs
+++ kms/root-server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::net::TcpListener;
+
+async fn hello() -> (StatusCode, String) {
+    (StatusCode::OK, "Hello, World!".to_owned())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(hello));
+
+    let listener = TcpListener::bind("0.0.0.0:1100")
+        .await
+        .context("failed to bind listener")?;
+    axum::serve(listener, app)
+        .await
+        .context("failed to serve")?;
+
+    Ok(())
 }
