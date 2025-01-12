e07fca0abfc149c974d574f62b648fe2a20cb78f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-12 16:17:52 +0530

    implement encryption

--- kms/root-server/src/generate.rs
+++ kms/root-server/src/generate.rs
@@ -1,4 +1,13 @@
+use alloy::{
+    primitives::keccak256,
+    signers::{local::PrivateKeySigner, SignerSync},
+};
+use anyhow::{Context, Result};
 use axum::{extract::State, http::StatusCode};
+use nucypher_core::{
+    encrypt_for_dkg, ferveo::api::DkgPublicKey, AccessControlPolicy, Conditions, ProtocolObject,
+    ThresholdMessageKit,
+};
 use rand::{rngs::OsRng, RngCore};
 
 use crate::AppState;
@@ -20,16 +29,62 @@ pub async fn generate(State(state): State<AppState>) -> (StatusCode, String) {
     OsRng.fill_bytes(randomness.as_mut());
 
     // generate encrypted message
-    let encrypted = vec![0u8; 512].into_boxed_slice();
-    // TODO: call nucypher
+    let Ok(encrypted) = encrypt(
+        &randomness,
+        &state.conditions,
+        state.dkg_public_key,
+        state.signer,
+    ) else {
+        // NOTE: Explicitly do not do anything with the error message
+        // lest it leaks something about the encryption process
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "failed to encrypt\n".into(),
+        );
+    };
 
     // set randomness and encrypted
     let mut randomness_guard = state.randomness.lock().unwrap();
     let mut encrypted_guard = state.encrypted.lock().unwrap();
     *randomness_guard = Some(randomness);
-    *encrypted_guard = encrypted;
+    *encrypted_guard = encrypted.clone();
     drop(encrypted_guard);
     drop(randomness_guard);
 
-    (StatusCode::OK, "Done\n".into())
+    (StatusCode::OK, encrypted)
+}
+
+fn encrypt(
+    message: &[u8],
+    conditions: &Conditions,
+    dkg_public_key: DkgPublicKey,
+    auth_signer: PrivateKeySigner,
+) -> Result<String> {
+    // encrypt
+    let (ciphertext, auth_data) =
+        encrypt_for_dkg(message, &dkg_public_key, &conditions).context("encrypt failed")?;
+
+    // calculate header hash
+    let header_hash = keccak256(
+        bincode::serialize(&ciphertext.header().context("failed to get header")?)
+            .context("failed to serialize header")?,
+    );
+
+    // sign the header hash
+    let authorization = auth_signer
+        .sign_message_sync(header_hash.as_slice())
+        .context("signing failed")?
+        .as_bytes()
+        .into();
+
+    // create access control policy
+    let acp = AccessControlPolicy {
+        auth_data,
+        authorization,
+    };
+
+    // create message kit
+    let message_kit = ThresholdMessageKit { ciphertext, acp };
+
+    Ok(hex::encode(message_kit.to_bytes()))
 }
