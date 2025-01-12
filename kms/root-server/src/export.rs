e3cb4aa968149ba36e15384b6cc8299a89648f56
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-12 16:41:33 +0530

    implement export

--- kms/root-server/src/export.rs
+++ kms/root-server/src/export.rs
@@ -3,5 +3,5 @@ use axum::{extract::State, http::StatusCode};
 use crate::AppState;
 
 pub async fn export(State(state): State<AppState>) -> (StatusCode, String) {
-    todo!()
+    (StatusCode::OK, state.encrypted.lock().unwrap().clone())
 }
