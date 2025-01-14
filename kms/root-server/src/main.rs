1965a09281e96d996310ae5d042d3850fcea4171
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-14 16:04:16 +0530

    add ritual taco nodes and threshold to state

--- kms/root-server/src/main.rs
+++ kms/root-server/src/main.rs
@@ -1,13 +1,21 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
-use alloy::signers::local::PrivateKeySigner;
+use alloy::{
+    primitives::{Address, U256},
+    providers::ProviderBuilder,
+    signers::local::PrivateKeySigner,
+    sol,
+};
 use anyhow::{Context, Result};
 use axum::{
     routing::{get, post},
     Router,
 };
 use clap::Parser;
-use nucypher_core::{ferveo::api::DkgPublicKey, Conditions};
+use nucypher_core::{ferveo::api::DkgPublicKey, Conditions, ProtocolObject, SessionStaticKey};
 use tokio::{fs::read, net::TcpListener};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
@@ -42,11 +50,15 @@ struct Args {
 
     /// Ritual id
     #[arg(long)]
-    ritual: String,
+    ritual: u32,
 
     /// Coordinator address
     #[arg(long)]
     coordinator: String,
+
+    /// RPC URL
+    #[arg(long)]
+    rpc: String,
 }
 
 #[derive(Clone)]
@@ -59,6 +71,10 @@ struct AppState {
     signer: PrivateKeySigner,
     conditions: Conditions,
     dkg_public_key: DkgPublicKey,
+    ritual: u32,
+    taco_nodes: HashMap<Address, SessionStaticKey>,
+    threshold: u16,
+    porter: String,
 }
 
 #[tokio::main]
@@ -68,11 +84,15 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     let signer = PrivateKeySigner::from_slice(
-        &read(args.signer)
+        &read(&args.signer)
             .await
             .context("failed to read signer file")?,
     )
     .context("failed to create signer")?;
+
+    let (taco_nodes, threshold) = get_taco_nodes(&args)
+        .await
+        .context("failed to fetch taco nodes")?;
 
     let app_state = AppState {
         signer,
@@ -83,6 +103,10 @@ async fn main() -> Result<()> {
             &hex::decode(args.dkg_public_key).context("failed to decode dkg public key")?,
         )
         .context("failed to create dkg public key")?,
+        ritual: args.ritual,
+        taco_nodes,
+        threshold,
+        porter: args.porter,
     };
 
     let app = Router::new()
@@ -102,6 +126,88 @@ async fn main() -> Result<()> {
         .context("failed to serve")?;
 
     Ok(())
+}
+
+sol! {
+    #[sol(rpc)]
+    #[derive(Debug)]
+    interface Coordinator {
+        struct G1Point {
+            bytes32 word0;
+            bytes16 word1;
+        }
+
+        struct Participant {
+            address provider;
+            bool aggregated;
+            bytes transcript;
+            bytes decryptionRequestStaticKey;
+            // Note: Adjust __postSentinelGap size if this struct's size changes
+        }
+
+        function rituals(
+            uint256 ritualId // uint256 for backward compatibility
+        )
+            external
+            view
+            returns (
+                address initiator,
+                uint32 initTimestamp,
+                uint32 endTimestamp,
+                uint16 totalTranscripts,
+                uint16 totalAggregations,
+                //
+                address authority,
+                uint16 dkgSize,
+                uint16 threshold,
+                bool aggregationMismatch,
+                //
+                address accessController,
+                G1Point memory publicKey,
+                bytes memory aggregatedTranscript,
+                address feeModel
+            );
+
+        function getParticipants(
+            uint32 ritualId,
+            uint256 startIndex,
+            uint256 maxParticipants,
+            bool includeTranscript
+        ) external view returns (Participant[] memory);
+    }
+}
+
+async fn get_taco_nodes(args: &Args) -> Result<(HashMap<Address, SessionStaticKey>, u16)> {
+    let provider =
+        ProviderBuilder::new().on_http(args.rpc.parse().context("failed to parse rpc url")?);
+    let contract = Coordinator::new(
+        args.coordinator
+            .parse()
+            .context("failed to parse coordinator address")?,
+        provider,
+    );
+
+    let ritual = contract
+        .rituals(U256::from(args.ritual))
+        .call()
+        .await
+        .context("failed to fetch ritual")?;
+
+    let participants = contract
+        .getParticipants(args.ritual, U256::ZERO, U256::from(ritual.dkgSize), false)
+        .call()
+        .await
+        .context("failed to get participants")?;
+
+    Ok((
+        HashMap::from_iter(participants._0.into_iter().filter_map(|p| {
+            Some((
+                p.provider,
+                SessionStaticKey::from_bytes(&p.decryptionRequestStaticKey).ok()?,
+            ))
+        })),
+        ritual.threshold,
+    ))
 }
 
 fn setup_logging() {
