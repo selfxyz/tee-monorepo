7e07e6942f9bcf744e9bff2d352f7425126d6df0
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-22 14:03:02 +0530

    fix import

--- contracts/contracts-foundry/test/kms/KmsRoot.t.sol
+++ contracts/contracts-foundry/test/kms/KmsRoot.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
 import {KmsRoot} from "../../src/kms/KmsRoot.sol";
 
 import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
