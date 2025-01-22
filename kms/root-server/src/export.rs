3c1f4e5cb3a678f7f1aff38fbfb1aaab97d00304
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-22 15:32:14 +0530

    clean up endpoints

--- kms/root-server/src/export.rs
+++
@@ -1,7 +0,0 @@
-use axum::{extract::State, http::StatusCode};
-
-use crate::AppState;
-
-pub async fn export(State(state): State<AppState>) -> (StatusCode, String) {
-    (StatusCode::OK, state.encrypted.lock().unwrap().clone())
-}
