8389ccac90d67d3f3235486a84aca0dbab10a3a7
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-23 16:06:42 +0530

    fix deps

--- kms/derive-server/default.nix
+++ kms/derive-server/default.nix
@@ -26,7 +26,7 @@ in rec {
     src = ./.;
     CARGO_BUILD_TARGET = target;
     TARGET_CC = "${cc}/bin/${cc.targetPrefix}cc";
-    nativeBuildInputs = [cc];
+    nativeBuildInputs = [cc pkgs.perl];
   };
 
   compressed =
