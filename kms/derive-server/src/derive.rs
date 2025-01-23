d85cd209cf72011633042357ebc6e2a2c9169c4f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-23 15:31:03 +0530

    derive key based on derivation path parameter

--- kms/derive-server/src/derive.rs
+++ kms/derive-server/src/derive.rs
@@ -1,5 +1,9 @@
-use axum::{extract::State, http::StatusCode};
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+};
 use hmac::{Hmac, Mac};
+use serde::Deserialize;
 use sha2::Sha512;
 
 use crate::AppState;
@@ -12,23 +16,20 @@ use crate::AppState;
 // new versions will need to obtain new randomness
 // the PCRs of the current version are the version
 
-// derive keys after verifying attestations
-pub async fn derive(State(state): State<AppState>) -> (StatusCode, [u8; 64]) {
-    // safe to unwrap since the server should always have an authstore
-    let (pcrs, user_data) = scallop_state.0.unwrap();
+#[derive(Deserialize)]
+struct Params {
+    path: String,
+}
 
-    if user_data.len() > 65535 {
-        return (StatusCode::BAD_REQUEST, [0; 64]);
-    }
-
+// derive keys based on derivation path
+pub async fn derive(
+    State(state): State<AppState>,
+    Query(params): Query<Params>,
+) -> (StatusCode, [u8; 64]) {
     let Ok(mut mac) = Hmac::<Sha512>::new_from_slice(&state.randomness) else {
         return (StatusCode::INTERNAL_SERVER_ERROR, [0; 64]);
     };
-    mac.update(&pcrs[0]);
-    mac.update(&pcrs[1]);
-    mac.update(&pcrs[2]);
-    mac.update(&(user_data.len() as u16).to_be_bytes());
-    mac.update(&user_data);
+    mac.update(params.path.as_bytes());
 
     let derived_key = mac.finalize().into_bytes().into();
 
