238e64d40e88495f99ac5ed6c99a3d6aa22ccbc8
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-24 19:25:52 +0530

    implement derive_enclave_seed

--- kms/derive-utils/src/lib.rs
+++ kms/derive-utils/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use hmac::Hmac;
+use hmac::Mac;
+use sha2::Sha512;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn derive_enclave_seed(
+    root: &[u8],
+    pcr0: &[u8],
+    pcr1: &[u8],
+    pcr2: &[u8],
+    user_data: &[u8],
+) -> Option<[u8; 64]> {
+    let mut mac = Hmac::<Sha512>::new_from_slice(root).ok()?;
+    mac.update(&pcr0);
+    mac.update(&pcr1);
+    mac.update(&pcr2);
+    mac.update(&(user_data.len() as u16).to_be_bytes());
+    mac.update(&user_data);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    Some(mac.finalize().into_bytes().into())
 }
