2563778b843197cf125bd20d6bec98b319faf74b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-24 19:30:18 +0530

    implement derive_path_seed

--- kms/derive-utils/src/lib.rs
+++ kms/derive-utils/src/lib.rs
@@ -3,18 +3,25 @@ use hmac::Mac;
 use sha2::Sha512;
 
 pub fn derive_enclave_seed(
-    root: &[u8],
+    root: [u8; 64],
     pcr0: &[u8],
     pcr1: &[u8],
     pcr2: &[u8],
     user_data: &[u8],
 ) -> Option<[u8; 64]> {
-    let mut mac = Hmac::<Sha512>::new_from_slice(root).ok()?;
+    let mut mac = Hmac::<Sha512>::new_from_slice(&root).ok()?;
     mac.update(&pcr0);
     mac.update(&pcr1);
     mac.update(&pcr2);
     mac.update(&(user_data.len() as u16).to_be_bytes());
     mac.update(&user_data);
+
+    Some(mac.finalize().into_bytes().into())
+}
+
+pub fn derive_path_seed(root: [u8; 64], path: &[u8]) -> Option<[u8; 64]> {
+    let mut mac = Hmac::<Sha512>::new_from_slice(&root).ok()?;
+    mac.update(&path);
 
     Some(mac.finalize().into_bytes().into())
 }
