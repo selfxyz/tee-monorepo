a1e8c09f503525613e40e6491e498ca119af1185
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-24 19:34:49 +0530

    implement as double hmac

--- kms/derive-utils/src/lib.rs
+++ kms/derive-utils/src/lib.rs
@@ -9,6 +9,26 @@ pub fn derive_enclave_seed(
     pcr2: &[u8],
     user_data: &[u8],
 ) -> Option<[u8; 64]> {
+    derive_enclave_seed_once(
+        derive_enclave_seed_once(root, pcr0, pcr1, pcr2, user_data)?,
+        pcr0,
+        pcr1,
+        pcr2,
+        user_data,
+    )
+}
+
+pub fn derive_path_seed(root: [u8; 64], path: &[u8]) -> Option<[u8; 64]> {
+    derive_path_seed_once(derive_path_seed_once(root, path)?, path)
+}
+
+fn derive_enclave_seed_once(
+    root: [u8; 64],
+    pcr0: &[u8],
+    pcr1: &[u8],
+    pcr2: &[u8],
+    user_data: &[u8],
+) -> Option<[u8; 64]> {
     let mut mac = Hmac::<Sha512>::new_from_slice(&root).ok()?;
     mac.update(&pcr0);
     mac.update(&pcr1);
@@ -19,7 +39,7 @@ pub fn derive_enclave_seed(
     Some(mac.finalize().into_bytes().into())
 }
 
-pub fn derive_path_seed(root: [u8; 64], path: &[u8]) -> Option<[u8; 64]> {
+fn derive_path_seed_once(root: [u8; 64], path: &[u8]) -> Option<[u8; 64]> {
     let mut mac = Hmac::<Sha512>::new_from_slice(&root).ok()?;
     mac.update(&path);
 
