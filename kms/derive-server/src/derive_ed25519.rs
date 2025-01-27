842d743a26a38d5524600d78da4278f72ee151d7
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-27 13:14:09 +0530

    add ed25519 derivation endpoint

---
+++ kms/derive-server/src/derive_ed25519.rs
@@ -0,0 +1,24 @@
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+};
+use kms_derive_utils::{derive_path_seed, to_ed25519_secret};
+use serde::Deserialize;
+
+use crate::AppState;
+
+#[derive(Deserialize)]
+pub struct Params {
+    path: String,
+}
+
+// derive keys based on derivation path
+pub async fn derive_ed25519(
+    State(state): State<AppState>,
+    Query(params): Query<Params>,
+) -> (StatusCode, [u8; 32]) {
+    let derived_key = derive_path_seed(state.randomness, params.path.as_bytes());
+    let secret = to_ed25519_secret(derived_key);
+
+    (StatusCode::OK, secret)
+}
