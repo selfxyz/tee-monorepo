c7e19a83e5ec96787527af8028be51b809abdceb
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-29 14:41:54 +0530

    bugfix: check public key to prevent attestation highjacking

--- kms/root-server/src/scallop.rs
+++ kms/root-server/src/scallop.rs
@@ -17,7 +17,7 @@ pub type AuthStoreState = ([[u8; 48]; 3], Box<[u8]>);
 impl ScallopAuthStore for AuthStore {
     type State = AuthStoreState;
 
-    fn verify(&mut self, attestation: &[u8], _key: Key) -> Option<Self::State> {
+    fn verify(&mut self, attestation: &[u8], key: Key) -> Option<Self::State> {
         let Ok(now) = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .map(|x| x.as_millis() as usize)
@@ -31,6 +31,7 @@ impl ScallopAuthStore for AuthStore {
                 // TODO: hardcoded, make it a param
                 age: Some((300000, now)),
                 root_public_key: Some(&AWS_ROOT_KEY),
+                public_key: Some(&key),
                 // do not care about PCRs or user data
                 // will derive different keys for each set
                 ..Default::default()
