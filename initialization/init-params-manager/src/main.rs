cca434490fc72658411fbdf96c31175eab25bfcc
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-01-31 14:39:36 +0530

    implement manager

--- initialization/init-params-manager/src/main.rs
+++ initialization/init-params-manager/src/main.rs
@@ -1,3 +1,156 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    convert::identity,
+    fs::{canonicalize, read_to_string, write},
+    path::Path,
+};
+
+use anyhow::{bail, Result};
+use base64::{prelude::BASE64_STANDARD, Engine};
+use clap::Parser;
+use libsodium_sys::{
+    crypto_box_MACBYTES, crypto_box_NONCEBYTES, crypto_box_open_easy, crypto_scalarmult_base,
+};
+use serde::Deserialize;
+use sha2::{Digest, Sha256};
+use tracing_subscriber::EnvFilter;
+
+#[derive(Parser)]
+struct Args {
+    /// Init params file
+    #[arg(long, default_value = "/app/init-params")]
+    init_params_path: String,
+
+    /// Derive server endpoint
+    #[arg(long, default_value = "http://127.0.0.1:1100")]
+    derive_endpoint: String,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .init();
+
+    // parse args
+    let args = Args::parse();
+
+    // parse init params
+    let init_params = serde_json::from_str::<InitParams>(&read_to_string(args.init_params_path)?)?;
+
+    // fetch key
+    let sk = fetch_encryption_key(&args.derive_endpoint)?;
+    let mut pk = [0u8; 32];
+
+    // SAFETY: pk and sk are the right size
+    // cannot fail, ignore return value
+    unsafe { crypto_scalarmult_base(pk.as_mut_ptr(), sk.as_ptr()) };
+
+    let hash = init_params
+        .into_iter()
+        .map(|init_param| -> Result<Option<[u8; 32]>> {
+            let path = canonicalize("/init-params/".to_owned() + &init_param.path)?;
+            // ensure all files are contained within this directory
+            if !path.starts_with(Path::new("/init-params/")) {
+                bail!("invalid path");
+            }
+
+            // we want to fail closed, hence detect errors as well
+            if path.try_exists()? {
+                bail!("file already exists");
+            }
+
+            let mut contents = BASE64_STANDARD.decode(init_param.contents.as_bytes())?;
+
+            if init_param.should_decrypt {
+                // contents is expected to contain the message at the beginning
+                // and then the mac and then the nonce
+                if contents.len() < (crypto_box_NONCEBYTES + crypto_box_MACBYTES) as usize
+                    || contents.len() > 65535
+                {
+                    bail!("invalid content length");
+                }
+
+                let clen = contents.len() as u64 - crypto_box_NONCEBYTES as u64;
+
+                // SAFETY: contents is big enough for the decrypted message
+                // in-place decryption is supported by libsodium
+                // pk and sk are the right size
+                let res = unsafe {
+                    crypto_box_open_easy(
+                        contents.as_mut_ptr(),
+                        contents.as_ptr(),
+                        clen,
+                        contents.as_ptr().offset(clen as isize),
+                        pk.as_ptr(),
+                        sk.as_ptr(),
+                    )
+                };
+                if res != 0 {
+                    bail!("failed to decrypt");
+                }
+
+                // successfully decrypted, truncate mac and nonce
+                contents.truncate(
+                    contents.len() - (crypto_box_NONCEBYTES + crypto_box_MACBYTES) as usize,
+                );
+            }
+
+            write(&path, &contents)?;
+
+            if init_param.should_attest {
+                let mut hasher = Sha256::new();
+                hasher.update(path.as_os_str().len().to_le_bytes());
+                hasher.update(path.as_os_str().as_encoded_bytes());
+                hasher.update(contents.len().to_le_bytes());
+                hasher.update(contents.as_slice());
+
+                return Ok(Some(hasher.finalize().into()));
+            } else {
+                return Ok(None);
+            }
+        })
+        // will short circuit if anything errors
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        // filter out Nones
+        .filter_map(identity)
+        // accumulate futher into a single hash
+        .fold(Sha256::new(), |mut hasher, param_hash| {
+            hasher.update(param_hash);
+            hasher
+        })
+        .finalize();
+
+    write("/app/init-params-digest", hash)?;
+
+    Ok(())
+}
+
+fn fetch_encryption_key(endpoint: &str) -> Result<[u8; 32]> {
+    Ok(ureq::get(endpoint.to_owned() + "/derive/x25519")
+        .query("path", "oyster.init-params")
+        .call()?
+        .body_mut()
+        .read_to_vec()?
+        .as_slice()
+        .try_into()?)
+}
+
+#[derive(Deserialize)]
+struct InitParam {
+    path: String,
+    contents: String, // base64 encoded
+    #[serde(default = "_default_true")]
+    should_attest: bool,
+    #[serde(default)]
+    should_decrypt: bool,
+}
+
+type InitParams = Vec<InitParam>;
+
+// blergh
+const fn _default_true() -> bool {
+    true
 }
