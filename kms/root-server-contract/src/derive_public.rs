8111dda507f8279b3912cd3127d87d8840eadd43
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-02-04 16:06:08 +0530

    update public function to derive based on contract

--- kms/root-server-contract/src/derive_public.rs
+++ kms/root-server-contract/src/derive_public.rs
@@ -3,7 +3,7 @@ use axum::{
     http::StatusCode,
 };
 use kms_derive_utils::{
-    derive_enclave_seed, derive_path_seed, to_ed25519_public, to_ed25519_solana_address,
+    derive_enclave_seed_contract, derive_path_seed, to_ed25519_public, to_ed25519_solana_address,
     to_secp256k1_ethereum_address, to_secp256k1_public, to_x25519_public,
 };
 use serde::Deserialize;
@@ -12,29 +12,14 @@ use crate::AppState;
 
 #[derive(Deserialize)]
 pub struct Params {
-    pcr0: String,
-    pcr1: String,
-    pcr2: String,
-    user_data: String,
+    chain_id: u64,
+    address: String,
     path: String,
 }
 
 impl Params {
     fn derive_path_seed(&self, randomness: [u8; 64]) -> Option<[u8; 64]> {
-        let Ok(pcr0) = hex::decode(&self.pcr0) else {
-            return None;
-        };
-        let Ok(pcr1) = hex::decode(&self.pcr1) else {
-            return None;
-        };
-        let Ok(pcr2) = hex::decode(&self.pcr2) else {
-            return None;
-        };
-        let Ok(user_data) = hex::decode(&self.user_data) else {
-            return None;
-        };
-
-        let enclave_key = derive_enclave_seed(randomness, &pcr0, &pcr1, &pcr2, &user_data);
+        let enclave_key = derive_enclave_seed_contract(randomness, self.chain_id, &self.address);
         let path_key = derive_path_seed(enclave_key, self.path.as_bytes());
 
         Some(path_key)
