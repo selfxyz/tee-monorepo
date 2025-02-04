4c60744cd857820eca554ec9d0ee9363adf9047b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-02-04 17:18:53 +0530

    implement key verification

--- kms/root-server-contract/src/derive.rs
+++ kms/root-server-contract/src/derive.rs
@@ -1,3 +1,4 @@
+use alloy::{providers::ProviderBuilder, sol};
 use axum::{
     extract::{ConnectInfo, Query, State},
     http::StatusCode,
@@ -5,8 +6,16 @@ use axum::{
 use kms_derive_utils::derive_enclave_seed_contract;
 use oyster::axum::ScallopState;
 use serde::Deserialize;
+use IKMSVerifiable::IKMSVerifiableInstance;
 
 use crate::{scallop::AuthStoreState, AppState};
+
+sol!(
+    #[sol(rpc)]
+    interface IKMSVerifiable {
+        function oyster_kms_verify(bytes32 _key) external returns (bool);
+    }
+);
 
 #[derive(Deserialize)]
 pub struct Params {
@@ -19,7 +28,28 @@ pub async fn derive(
     State(state): State<AppState>,
     Query(params): Query<Params>,
 ) -> (StatusCode, [u8; 64]) {
-    // TODO: Verify if key is verified on the contract
+    let Ok(address) = params.address.parse() else {
+        return (StatusCode::BAD_REQUEST, [0; 64]);
+    };
+    let Ok(rpc) = state.rpc.parse() else {
+        return (StatusCode::BAD_REQUEST, [0; 64]);
+    };
+    let provider = ProviderBuilder::new().on_http(rpc);
+    let contract = IKMSVerifiableInstance::new(address, provider);
+
+    // SAFETY: transport should always have key associated, safe to unwrap
+    let Ok(res) = contract
+        .oyster_kms_verify(scallop_state.0.unwrap().into())
+        .call()
+        .await
+    else {
+        return (StatusCode::INTERNAL_SERVER_ERROR, [0; 64]);
+    };
+
+    // error if key is not verified
+    if !res._0 {
+        return (StatusCode::UNAUTHORIZED, [0; 64]);
+    }
 
     let derived_key =
         derive_enclave_seed_contract(state.randomness, state.chain_id, &params.address);
