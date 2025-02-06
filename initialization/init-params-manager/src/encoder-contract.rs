e94b61a93590ed9d272296280034e0665a5c4dfb
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-02-06 10:38:40 +0530

    use a single pass to compute params and digest, key is independent of digest

--- initialization/init-params-manager/src/encoder-contract.rs
+++ initialization/init-params-manager/src/encoder-contract.rs
@@ -49,68 +49,12 @@ fn run() -> Result<()> {
         bail!("failed to init libsodium");
     }
 
-    // process in two passes since digest is needed to fetch public key
-
-    // compute digest
-    let digest = args
-        .init_params
-        .iter()
-        .map(|param| {
-            // extract components
-            let param_components = param.splitn(5, ":").collect::<Vec<_>>();
-            let should_attest = param_components[1] == "1";
-
-            if !should_attest {
-                return Ok(None);
-            }
-
-            // everything should be normal components, no root or current or parent dirs
-            if PathBuf::from(param_components[0])
-                .components()
-                .any(|x| !matches!(x, Component::Normal(_)))
-            {
-                bail!("invalid path")
-            }
-
-            let enclave_path = PathBuf::from("/init-params/".to_owned() + param_components[0]);
-            let should_encrypt = param_components[2] == "1";
-            let contents = match param_components[3] {
-                "utf8" => param_components[4].as_bytes().to_vec(),
-                "file" => fs::read(param_components[4]).context("failed to read file")?,
-                _ => bail!("unknown param type"),
-            };
-
-            info!(
-                path = param_components[0],
-                should_attest, should_encrypt, "digest"
-            );
-
-            // compute individual digest
-            let mut hasher = Sha256::new();
-            hasher.update(enclave_path.as_os_str().len().to_le_bytes());
-            hasher.update(enclave_path.as_os_str().as_encoded_bytes());
-            hasher.update(contents.len().to_le_bytes());
-            hasher.update(contents);
-
-            Ok(Some(hasher.finalize()))
-        })
-        .collect::<Result<Vec<_>>>()
-        .context("failed to compute individual digest")?
-        .into_iter()
-        .flatten()
-        // accumulate futher into a single hash
-        .fold(Sha256::new(), |mut hasher, param_hash| {
-            hasher.update(param_hash);
-            hasher
-        })
-        .finalize();
-
     // fetch key
     let pk =
         fetch_encryption_key(&args.kms_endpoint, &args.address).context("failed to fetch key")?;
 
     // prepare init params
-    let params = args
+    let (params, digests): (Vec<_>, Vec<_>) = args
         .init_params
         .iter()
         .map(|param| {
@@ -128,6 +72,29 @@ fn run() -> Result<()> {
                 path = param_components[0],
                 should_attest, should_encrypt, "param"
             );
+
+            // everything should be normal components, no root or current or parent dirs
+            if PathBuf::from(param_components[0])
+                .components()
+                .any(|x| !matches!(x, Component::Normal(_)))
+            {
+                bail!("invalid path")
+            }
+            let enclave_path = PathBuf::from("/init-params/".to_owned() + param_components[0]);
+
+            // compute digest
+            let digest = if should_attest {
+                // compute individual digest
+                let mut hasher = Sha256::new();
+                hasher.update(enclave_path.as_os_str().len().to_le_bytes());
+                hasher.update(enclave_path.as_os_str().as_encoded_bytes());
+                hasher.update(contents.len().to_le_bytes());
+                hasher.update(&contents);
+
+                Some(hasher.finalize())
+            } else {
+                None
+            };
 
             // encrypt if needed
             let final_contents = if should_encrypt {
@@ -154,10 +121,22 @@ fn run() -> Result<()> {
                 should_decrypt: should_encrypt,
             };
 
-            Ok(init_param)
+            Ok((init_param, digest))
         })
         .collect::<Result<Vec<_>>>()
-        .context("failed to build init params")?;
+        .context("failed to build init params")?
+        .into_iter()
+        .unzip();
+
+    let digest = digests
+        .into_iter()
+        .flatten()
+        // accumulate futher into a single hash
+        .fold(Sha256::new(), |mut hasher, param_hash| {
+            hasher.update(param_hash);
+            hasher
+        })
+        .finalize();
 
     // create final init params
     let init_params = InitParamsList {
