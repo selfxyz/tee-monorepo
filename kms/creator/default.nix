e08f271371bdc9bd1cc6e3210cbbb5561b0ff71d
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-04-04 14:18:15 +0530

    fix kms creator build

--- kms/creator/default.nix
+++ kms/creator/default.nix
@@ -21,9 +21,24 @@
     if systemConfig.static
     then pkgs.pkgsStatic.stdenv.cc
     else pkgs.stdenv.cc;
+  projectSrc = ./.;
+  libSrc = ../derive-utils;
+  combinedSrc = pkgs.runCommand "combined-src" {} ''
+    # Copy the project
+    cp -r ${projectSrc} $out
+    chmod -R +w $out
+
+    # Copy the library into the project directory
+    mkdir -p $out/libs/derive-utils
+    cp -r ${libSrc}/* $out/libs/derive-utils
+
+    # Patch Cargo.toml to point to the new library location
+    substituteInPlace $out/Cargo.toml \
+      --replace 'path = "../derive-utils"' 'path = "./libs/derive-utils"'
+  '';
 in rec {
   uncompressed = naersk'.buildPackage {
-    src = ./.;
+    src = combinedSrc;
     CARGO_BUILD_TARGET = target;
     TARGET_CC = "${cc}/bin/${cc.targetPrefix}cc";
     nativeBuildInputs = [cc];
