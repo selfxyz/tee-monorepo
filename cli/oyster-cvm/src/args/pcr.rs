310260be603aa8081cf6d6a2a5fca24c3c2a4823
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-04-11 13:36:01 +0530

    add v2 pcrs

--- cli/oyster-cvm/src/args/pcr.rs
+++ cli/oyster-cvm/src/args/pcr.rs
@@ -72,6 +72,16 @@ impl PcrArgs {
 
         if let Some(ref name) = self.pcr_preset.or(default_preset) {
             return match name.as_str() {
+                "base/blue/v2.0.0/amd64" => Ok(Some((
+                    PCRS_BASE_BLUE_V2_0_0_AMD64.0.into(),
+                    PCRS_BASE_BLUE_V2_0_0_AMD64.1.into(),
+                    PCRS_BASE_BLUE_V2_0_0_AMD64.2.into(),
+                ))),
+                "base/blue/v2.0.0/arm64" => Ok(Some((
+                    PCRS_BASE_BLUE_V2_0_0_ARM64.0.into(),
+                    PCRS_BASE_BLUE_V2_0_0_ARM64.1.into(),
+                    PCRS_BASE_BLUE_V2_0_0_ARM64.2.into(),
+                ))),
                 "base/blue/v1.0.0/amd64" => Ok(Some((
                     PCRS_BASE_BLUE_V1_0_0_AMD64.0.into(),
                     PCRS_BASE_BLUE_V1_0_0_AMD64.1.into(),
@@ -113,6 +123,18 @@ pub fn preset_to_pcr_preset(preset: &str, arch: &Platform) -> Option<String> {
     }
     .map(str::to_owned)
 }
+
+pub static PCRS_BASE_BLUE_V2_0_0_AMD64: (&str, &str, &str) = (
+    "862b295eefd03db5132be44464d8280728ccd7cc4513284ad4e9dfa923c93e8df2ebcf37b8728f2de75e48e0ea1d2c1c",
+    "ed7759aa996a2e94c6086f24f61f354f75f9ea7f93a74f55d65c2cb5590d1af3930c9adbc57bb543764fa1f5c444f495",
+    "ca759b0a285727e01471c49a42a3ca079f480963f90cc73b28e645ff95d85f006c149561970a54053e0e8a51d7c288ca",
+);
+
+pub static PCRS_BASE_BLUE_V2_0_0_ARM64: (&str, &str, &str) = (
+    "8ef3a8e2631d6fa53e0a75e7a38072df2da40fe3d9b777c6673a738aa8df5e21bfdc772c8935f12567b10fd10353e7aa",
+    "b0d319fa64f9c2c9d7e9187bc21001ddacfab4077e737957fa1b8b97cc993bed43a79019aebfd40ee5f6f213147909f8",
+    "3daa3181531f1d80c00ef5ff846e0ff8ed5298dff3472fb9fbaf1bb76f54452937f6645371f01f492cabf1793517c361",
+);
 
 pub static PCRS_BASE_BLUE_V1_0_0_AMD64: (&str, &str, &str) = (
     "181023664fd6477acdb28bb3d7b7e5eff6001a7a8c2d32309e076460fa6cda213cee6c4c0b97c96421bf6b1b74305030",
