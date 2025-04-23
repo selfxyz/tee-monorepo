111e90ae24fa52749bef04027f7d5364282217bc
sagarparker <sagar8parker@gmail.com>
2025-04-23 12:19:34 +0530

    Added docker setup for running workerd

--- cli/oyster-serverless/src/commands/new_project.rs
+++ cli/oyster-serverless/src/commands/new_project.rs
@@ -1,15 +1,15 @@
-use anyhow::{Result, Context};
+use anyhow::{Context, Result};
 use clap::Args;
-use std::fs;
-use tracing::info;
-use inquire::Select;
+use inquire::{Select, Text};
 use reqwest;
+use std::fs;
 use std::io::Write;
+use tracing::info;
 
 #[derive(Args)]
 pub struct NewArgs {
     /// Name of the project to create
-    #[arg(short, long, required=true)]
+    #[arg(short, long, required = true)]
     name: String,
 }
 
@@ -28,15 +28,23 @@ impl std::fmt::Display for Template {
     }
 }
 
-async fn download_template(template: &Template, project_path: &std::path::Path) -> Result<()> {
-    let base_url = "https://raw.githubusercontent.com/marlinprotocol/oyster-serverless-templates/master";
-    
-    let template_url = match template {
-        Template::HelloWorld => format!("{}/hello-world/worker.js", base_url),
-        Template::EthPrice => format!("{}/eth-price/worker.js", base_url),
-    };
+#[derive(Clone)]
+enum TemplateSource {
+    Predefined,
+    GithubUrl,
+}
 
-    let response = reqwest::get(&template_url)
+impl std::fmt::Display for TemplateSource {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TemplateSource::Predefined => write!(f, "Select from predefined templates"),
+            TemplateSource::GithubUrl => write!(f, "Provide GitHub template URL"),
+        }
+    }
+}
+
+async fn download_template(template_url: &str, project_path: &std::path::Path) -> Result<()> {
+    let response = reqwest::get(template_url)
         .await
         .context("Failed to download template")?
         .text()
@@ -44,9 +52,8 @@ async fn download_template(template: &Template, project_path: &std::path::Path) 
         .context("Failed to read template content")?;
 
     let worker_path = project_path.join("worker.js");
-    let mut file = fs::File::create(&worker_path)
-        .context("Failed to create worker.js")?;
-    
+    let mut file = fs::File::create(&worker_path).context("Failed to create worker.js")?;
+
     file.write_all(response.as_bytes())
         .context("Failed to write template content")?;
 
@@ -56,15 +63,32 @@ async fn download_template(template: &Template, project_path: &std::path::Path) 
 pub async fn run_new(args: NewArgs) -> Result<()> {
     let project_path = std::env::current_dir()?.join(&args.name);
     fs::create_dir_all(&project_path)?;
-    
-    let templates = vec![Template::HelloWorld, Template::EthPrice];
-    
-    let template = Select::new("Select a starter template:", templates)
-        .prompt()
-        .context("Failed to select template")?;
 
-    download_template(&template, &project_path).await?;
-    
+    let sources = vec![TemplateSource::Predefined, TemplateSource::GithubUrl];
+
+    let source = Select::new("Choose template source:", sources)
+        .prompt()
+        .context("Failed to select template source")?;
+
+    let template_url = match source {
+        TemplateSource::Predefined => {
+            let templates = vec![Template::HelloWorld, Template::EthPrice];
+            let template = Select::new("Select a starter template:", templates)
+                .prompt()
+                .context("Failed to select template")?;
+
+            let base_url = "https://raw.githubusercontent.com/marlinprotocol/oyster-serverless-templates/master";
+            match template {
+                Template::HelloWorld => format!("{}/hello-world/worker.js", base_url),
+                Template::EthPrice => format!("{}/eth-price/worker.js", base_url),
+            }
+        }
+        TemplateSource::GithubUrl => Text::new("Enter GitHub raw URL for the template:")
+            .prompt()
+            .context("Failed to get template URL")?,
+    };
+
+    download_template(&template_url, &project_path).await?;
     info!("Created new project at {}", project_path.display());
     Ok(())
 }
