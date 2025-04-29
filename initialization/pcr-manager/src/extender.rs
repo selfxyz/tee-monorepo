20ea5f5f20579f9f8d965bada2ab633bea79ff8f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-04-29 13:03:39 +0530

    add cli args

--- initialization/pcr-manager/src/extender.rs
+++ initialization/pcr-manager/src/extender.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+/// Extend PCRs
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// PCR index, should be within [16, 31] inclusive
+    #[arg(short, long)]
+    index: String,
+
+    /// path to file whose contents to extend the PCR with
+    #[arg(short, long)]
+    contents_path: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
