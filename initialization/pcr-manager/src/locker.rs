9d70597e79d60b7a6a2f8a8d69a4762f54e4aadc
Roshan Raghupathy <thisisroshansmail@gmail.com>
2025-04-29 13:22:55 +0530

    implement locker

--- initialization/pcr-manager/src/locker.rs
+++ initialization/pcr-manager/src/locker.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Result, bail};
+use aws_nitro_enclaves_nsm_api::{
+    api::{Request, Response},
+    driver::{nsm_exit, nsm_init, nsm_process_request},
+};
+use clap::Parser;
+
+/// Lock PCRs
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// PCR index, should be within [16, 31] inclusive
+    #[arg(short, long, value_parser = 16..32)]
+    index: u8,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let req = Request::LockPCR {
+        index: args.index.into(),
+    };
+
+    let nsm_fd = nsm_init();
+    if nsm_fd < 0 {
+        bail!("failed to initialize nsm");
+    }
+
+    let resp = nsm_process_request(nsm_fd, req);
+    nsm_exit(nsm_fd);
+    if let Response::Error(e) = resp {
+        bail!("failed to lock pcr: {e:?}");
+    };
+
+    Ok(())
 }
