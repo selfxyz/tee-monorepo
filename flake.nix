7de75afa4618402fcf9a6179fab5234ec60e61ae
ayman <aymanshaik1015@gmail.com>
2025-05-15 08:33:03 +0000

    chore: add necesary buildable packages

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   nixConfig = {
+    allow-import-from-derivation = true;
     extra-substituters = ["https://oyster.cachix.org"];
     extra-trusted-public-keys = ["oyster.cachix.org-1:QEXLEQvMA7jPLn4VZWVk9vbtypkXhwZknX+kFgDpYQY="];
   };
@@ -35,9 +36,6 @@
       attestation-server = import ./attestation-server {
         inherit nixpkgs systemConfig fenix naersk;
       };
-      initialization.init-server = import ./initialization/init-server {
-        inherit nixpkgs systemConfig fenix naersk;
-      };
       initialization.vet = import ./initialization/vet {
         inherit nixpkgs systemConfig fenix naersk;
       };
@@ -51,10 +49,9 @@
   in {
     formatter = {
       "x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
-      "aarch64-linux" = nixpkgs.legacyPackages."aarch64-linux".alejandra;
     };
     packages = {
-      "x86_64-linux" = rec {
+      "x86_64-linux" = let
         gnu = systemBuilder {
           system = "x86_64-linux";
           rust_target = "x86_64-unknown-linux-gnu";
@@ -67,38 +64,13 @@
           eif_arch = "x86_64";
           static = true;
         };
-        default = musl;
-      };
-      "aarch64-linux" = rec {
-        gnu = systemBuilder {
-          system = "aarch64-linux";
-          rust_target = "aarch64-unknown-linux-gnu";
-          eif_arch = "aarch64";
-          static = false;
-        };
-        musl = systemBuilder {
-          system = "aarch64-linux";
-          rust_target = "aarch64-unknown-linux-musl";
-          eif_arch = "aarch64";
-          static = true;
-        };
-        default = musl;
-      };
-      "aarch64-darwin" = rec {
-        gnu = systemBuilder {
-          system = "aarch64-darwin";
-          rust_target = "aarch64-apple-darwin";
-          eif_arch = "aarch64";
-          static = false;
-        };
-        # TODO: Figure out how to organize this properly
-        musl = systemBuilder {
-          system = "aarch64-darwin";
-          rust_target = "aarch64-apple-darwin";
-          eif_arch = "aarch64";
-          static = false;
-        };
-        default = musl;
+      in {
+        attestation-server = musl.attestation-server.default;
+        dnsproxy = musl.external.dnsproxy.default;
+        supervisord = musl.external.supervisord.default;
+        vet = musl.initialization.vet.default;
+        tuna = musl.kernels.tuna.default;
+        raw-proxy = musl.networking.raw-proxy.default;
       };
     };
   };
