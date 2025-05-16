611ea908465119cdde403d315550c6dfb23d5a09
sagarparker <sagar8parker@gmail.com>
2025-05-16 18:10:00 +0530

    Changes in new and deploy command for new code contract

--- cli/oyster-serverless/src/configs/global.rs
+++ cli/oyster-serverless/src/configs/global.rs
@@ -1,12 +1,14 @@
+// TODO : Change contract address for code contract to mainnet and RPC URL to mainnet
+
 // Oyster Serverless Contract Addresses
 pub const OYSTER_SERVERLESS_CODE_CONTRACT_ADDRESS: &str =
-    "0x71C8C416fa8c78E58d7b6BC3c6EBC8EE93C0FC80";
+    "0xE10F9D50eDEef610888e75fA4BC82f8dA14167B2";
 
 pub const RELAY_CONTRACT_ADDRESS: &str = "0xD28179711eeCe385bc2096c5D199E15e6415A4f5";
 pub const RELAY_SUBSCRIPTIONS_CONTRACT_ADDRESS: &str = "0x8Fb2C621d6E636063F0E49828f4Da7748135F3cB";
 pub const USDC_ADDRESS: &str = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 
 // RPC URLs
-pub const ARBITRUM_ONE_RPC_URL: &str = "https://arb1.arbitrum.io/rpc";
+pub const ARBITRUM_ONE_RPC_URL: &str = "https://sepolia-rollup.arbitrum.io/rpc";
 
 pub const INDEXER_URL: &str = "https://graph.lagoon.build/serverless/graphql";
